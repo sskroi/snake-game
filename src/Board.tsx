cd1304c2aabe40f5c2e772a9adadb16863a4ea30
Andrei <sskroi200ok@gmail.com>
2025-06-17 19:50:35 +0300

    add touch control support

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,9 +1,9 @@
-import type { FC } from "react";
+import type { FC, HTMLAttributes } from "react";
 import styles from "./Board.module.css";
 import type { CellType } from "./snakeEngine";
 
-interface BoardProps {
-  grid: CellType[][];
+interface BoardProps extends HTMLAttributes<HTMLDivElement> {
+  grid: CellType[][] | undefined;
 }
 
 interface FlatCell {
@@ -12,15 +12,17 @@ interface FlatCell {
   type: CellType;
 }
 
-export const Board: FC<BoardProps> = ({ grid }) => {
+export const Board: FC<BoardProps> = ({ grid, ...props }) => {
   const flatCells: FlatCell[] = [];
 
-  for (let i = 0; i < grid.length; i++) {
-    flatCells.push(...grid[i].map((v, j) => ({ row: i, col: j, type: v })));
+  if (grid) {
+    for (let i = 0; i < grid.length; i++) {
+      flatCells.push(...grid[i].map((v, j) => ({ row: i, col: j, type: v })));
+    }
   }
 
   return (
-    <div className={styles.board}>
+    <div className={styles.board} {...props}>
       {flatCells.map((v) => (
         <div
           key={`r${v.row}c${v.col}`}
--- src/SnakeGame.tsx
+++ src/SnakeGame.tsx
@@ -6,39 +6,36 @@ import { TransparentModal } from "./ui/TransparentModal";
 
 export const SnakeGame = () => {
   const engine = useRef<SnakeEngine>(null);
-  if (engine.current === null) {
-    engine.current = new SnakeEngine();
-  }
-  const [grid, setGrid] = useState(() => engine.current!.getGrid());
-  const [gameStatus, setGameStatus] = useState(() =>
-    engine.current!.getStatus(),
-  );
+  const [grid, setGrid] = useState(() => engine.current?.getGrid());
+  const [gameStatus, setGameStatus] = useState("");
   const [paused, setPaused] = useState(true);
   const [bestScore, setBestScore] = useState(() =>
     localStorage.getItem("bestScore"),
   );
 
+  const touchData = useRef({ startX: -1, startY: -1 });
+
   useEffect(() => {
     const keyDownHandler = (ev: KeyboardEvent) => {
       switch (ev.key) {
-        case "ArrowUp":
-          engine.current!.changeDirection("up");
-          break;
-        case "ArrowDown":
-          engine.current!.changeDirection("down");
-          break;
-        case "ArrowLeft":
-          engine.current!.changeDirection("left");
-          break;
-        case "ArrowRight":
-          engine.current!.changeDirection("right");
-          break;
         case " ":
           if (paused) {
             engine.current = new SnakeEngine();
             setGameStatus(engine.current.getStatus());
             setPaused(false);
           }
+          break;
+        case "ArrowUp":
+          engine.current?.changeDirection("up");
+          break;
+        case "ArrowDown":
+          engine.current?.changeDirection("down");
+          break;
+        case "ArrowLeft":
+          engine.current?.changeDirection("left");
+          break;
+        case "ArrowRight":
+          engine.current?.changeDirection("right");
           break;
       }
     };
@@ -49,18 +46,22 @@ export const SnakeGame = () => {
 
   useEffect(() => {
     const tickHandler = () => {
-      if (engine.current!.getStatus() !== "gameOver" && !paused) {
-        const status = engine.current!.nextTick();
+      if (
+        engine.current &&
+        engine.current.getStatus() !== "gameOver" &&
+        !paused
+      ) {
+        const status = engine.current.nextTick();
         if (status === "gameOver") {
           setPaused(true);
 
-          const score = engine.current!.getScore();
+          const score = engine.current.getScore();
           if (bestScore === null || score > +bestScore) {
             setBestScore(score.toString());
             localStorage.setItem("bestScore", score.toString());
           }
         }
-        setGrid(engine.current!.getGrid());
+        setGrid(engine.current.getGrid());
         setGameStatus(status);
       }
     };
@@ -69,16 +70,54 @@ export const SnakeGame = () => {
     return () => clearInterval(interval);
   }, [paused, bestScore]);
 
+  const touchStartHandler = (ev: React.TouchEvent) => {
+    const touch = ev.changedTouches[0];
+    touchData.current.startX = touch.clientX;
+    touchData.current.startY = touch.clientY;
+  };
+
+  const touchEndHandler = (ev: React.TouchEvent) => {
+    if (paused) {
+      engine.current = new SnakeEngine();
+      setGameStatus(engine.current.getStatus());
+      setPaused(false);
+    }
+
+    const touch = ev.changedTouches[0];
+    const dx = touch.clientX - touchData.current.startX;
+    const dy = touch.clientY - touchData.current.startY;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+      if (dx > 10) {
+        engine.current?.changeDirection("right");
+      } else if (dx < -10) {
+        engine.current?.changeDirection("left");
+      }
+    } else {
+      if (dy > 10) {
+        engine.current?.changeDirection("down");
+      } else if (dy < -10) {
+        engine.current?.changeDirection("up");
+      }
+    }
+  };
+
   return (
-    <main className={styles.snakeGame}>
+    <main
+      className={styles.snakeGame}
+      onTouchStart={touchStartHandler}
+      onTouchEnd={touchEndHandler}
+      style={{ touchAction: "none" }}
+      onTouchMove={(ev) => ev.preventDefault()}
+    >
       <Board grid={grid} />
-      <div className={styles.score}>SCORE: {engine.current!.getScore()}</div>
+      <div className={styles.score}>SCORE: {engine.current?.getScore()}</div>
       <TransparentModal open={paused}>
         <div className={styles.modalText}>
           {gameStatus === "gameOver" && (
             <>
               <p>GAME OVER </p>
-              <p>SCORE: {engine.current!.getScore()}</p>
+              <p>SCORE: {engine.current?.getScore()}</p>
               {bestScore !== undefined && <p>BEST SCORE: {bestScore}</p>}
             </>
           )}
